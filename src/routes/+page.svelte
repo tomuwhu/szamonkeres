f7d4ee269fb283762d4906b1d8f372755a7ac7fd
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-02-19 17:11:55 +0100

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     const { data } = $props()
     let user = $state(data.user)
 </script>
-<h1>Céges eszköz nyilvántartó program</h1>
+<h1>Céges eszköz-nyilvántartó program</h1>
 {#if user}
     <p>Bejelentkezve: {user.name}</p>
 {:else}
