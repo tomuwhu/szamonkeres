c6f861fc0510c5accf7f818f6ab1f817c95180c6
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-02-22 12:17:23 +0100

    Hiba kijavítva

--- src/routes/tools/+page.server.js
+++ src/routes/tools/+page.server.js
@@ -1,15 +1,19 @@
 import { redirect } from '@sveltejs/kit';
 import { db } from '$lib/server/db'
-import { eq } from 'drizzle-orm'
+import { eq, or } from 'drizzle-orm'
 import * as table from '$lib/server/db/schema'
 export const load = async (event) => {
     var toolslist = await db.select()
         .from(table.tools)
         .leftJoin(table.user, eq(table.user.id, table.tools.user_id)).all()
+    var userlist = await db.select({ id: table.user.id, name: table.user.name })
+        .from(table.user)
+        .all()
+    console.log(userlist);
     if (!event.locals.user) {
         return redirect(302, '/demo/lucia/login')
     }
-    return { user: event.locals.user, toolslist }
+    return { user: event.locals.user, toolslist, userlist }
 }
 
 export const actions = {
@@ -19,7 +23,7 @@ export const actions = {
         const description = formData.get('description');
         const image = formData.get('image');
         const serialnum = formData.get('serialnum');
-        const userId = event.locals.user.id;
+        const userId = formData.get('user');
         try {
             await db.insert(table.tools).values({ title, description, image, serialnum, user_id: userId }).returning();
         } catch (e) {
--- src/routes/tools/+page.svelte
+++ src/routes/tools/+page.svelte
@@ -1,100 +1,134 @@
 <script>
-    const { data } = $props()
-    console.log(data.toolslist);
-    let urlap = $state(0)
-    let kepnyitva = $state(0)    
+  const { data } = $props();
+  let urlap = $state(0);
+  let kepnyitva = $state(0);
+  let edittool = $state({});
+  console.log(data.userlist);
 </script>
+
 <h1>Eszközök ({data.user?.name})</h1>
 {#if urlap === 0}
-<button class="ui green small button" onclick={() => urlap=1}>Uj eszköz</button>
-{/if}
-{#if urlap === 1}
-<button class="ui blue small button" onclick={() => urlap=0}>Űrlap bezárása (mentés nélkül)</button>
-<hr>
-<form action="?/ujeszkoz" method="POST">
-        <div class="ui input">
-            <div class="ui label">Megnevezés</div>
-            <input type="text" name="title" />
-        </div>
-        <div class="ui input">
-            <div class="ui label">Leírás</div>
-            <input type="text" name="description" />
-        </div>
-        <div class="ui input">
-            <div class="ui label">Kép link</div>
-            <input type="text" name="image" />
-        </div>
-        <div class="ui input">
-            <div class="ui label">Sorozatszám</div>
-            <input type="text" name="serialnum" />
-        </div>
-        <div class="ui input">
-            <div class="ui label">Felhasználó</div>
-            <select class="ui dropdown" name="user">
-                <option value={data.user.id}>{data.user.name}</option>
-            </select>
-        </div>
-        <div class="ui divider"></div>
-        <button class="ui green small button">Mentés</button>
-    </form>
+  <button
+    class="ui green small button"
+    onclick={() => ((urlap = 1), (edittool = {}))}>Uj eszköz</button
+  >
+{:else}
+  <button class="ui blue small button" onclick={() => (urlap = 0)}
+    >Űrlap bezárása (mentés nélkül)</button
+  >
+  <hr />
+  <form action="?/ujeszkoz" method="POST">
+    <div class="ui input">
+      <div class="ui label">Megnevezés</div>
+      <input type="text" name="title" value={edittool.tools?.title} />
+    </div>
+    <div class="ui input">
+      <div class="ui label">Leírás</div>
+      <input
+        type="text"
+        name="description"
+        value={edittool.tools?.description}
+      />
+    </div>
+    <div class="ui input">
+      <div class="ui label">Kép link</div>
+      <input type="text" name="image" value={edittool.tools?.image} />
+    </div>
+    <div class="ui input">
+      <div class="ui label">Sorozatszám</div>
+      <input type="text" name="serialnum" value={edittool.tools?.serialnum} />
+    </div>
+    <div class="ui input">
+      <div class="ui label">Felhasználó</div>
+      <select class="ui dropdown" name="user" value={edittool.user?.id}>
+        <option value="bea7vrfcextbcweh75lesymc">Nincs még senkinél</option>
+        <option value={data.user.id}>{data.user.name}</option>
+      </select>
+    </div>
+    <div class="ui divider"></div>
+    <button class="ui green small button">Mentés</button>
+  </form>
 {/if}
 <table class="ui table">
-    <thead>
-        <tr class="cica">
-            <th>Megnevezés</th>
-            <th>Leírás</th>
-            <th>Sorozatszám</th>
-            <th>Kinél van</th>
-        </tr>
-    </thead>
-    <tbody>
-        {#each data.toolslist as tool}
-        <tr class="ez {kepnyitva === tool.tools.id ? 'active' : ''}" 
-        onclick="{() => kepnyitva=kepnyitva!==tool.tools.id?tool.tools.id:0}">
-            <td>{tool.tools.title}</td>
-            <td>{tool.tools.description}</td>
-            <td>{tool.tools.serialnum}</td>
-            <td>{tool.user.name}</td>
-        </tr>
-        {#if kepnyitva === tool.tools.id}
+  <thead>
+    <tr class="cica">
+      <th>Megnevezés</th>
+      <th>Leírás</th>
+      <th>Sorozatszám</th>
+      <th>Kinél van</th>
+      <th>Funkciók</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each data.toolslist as tool}
+      <tr
+        class="ez {kepnyitva === tool.tools.id ? 'active' : ''}"
+        onclick={() =>
+          (kepnyitva = kepnyitva !== tool.tools.id ? tool.tools.id : 0)}
+      >
+        <td>{tool.tools?.title}</td>
+        <td>{tool.tools?.description}</td>
+        <td>{tool.tools?.serialnum}</td>
+        <td>{tool.user?.name}</td>
+        <td
+          ><span
+            onclick={() => {
+              urlap = 2;
+              edittool = tool;
+              console.log(edittool);
+            }}
+            class="edit">✍</span
+          ></td
+        >
+      </tr>
+      {#if urlap == 0 && kepnyitva === tool.tools.id}
         <tr>
-            <td colspan="3" class="center">
-                <img src="{tool.tools.image}" alt="{tool.tools.description}">
-            </td>
+          <td colspan="3" class="center">
+            <img src={tool.tools.image} alt={tool.tools.description} />
+          </td>
         </tr>
-        {/if}
+      {/if}
     {/each}
-    </tbody>
+  </tbody>
 </table>
+
 <style>
-    tr.ez:hover {
-        background-color: lightcyan;
-    }
-    tr.active td {
-        background-color: lightcoral;
-    }
-    td.center {
-        text-align: center;
-    }
-    tr {
-        user-select: none;
-        cursor: pointer;
-    }
-    img {
-        height: 500px;
-    }
-    input, select {
-        width: 300px;
-    }
-    div.label {
-        width: 100px;
-        text-align: left;
-    }
-    form {
-        display: inline-block;
-        width: 400px;
-    }
-    tr.cica th {
-        background-color: rgb(128, 201, 240);
-    }
+  tr.ez:hover {
+    background-color: lightcyan;
+  }
+  tr.active td {
+    background-color: lightcoral;
+  }
+  td.center {
+    text-align: center;
+  }
+  tr {
+    user-select: none;
+    cursor: pointer;
+  }
+  img {
+    height: 500px;
+  }
+  input,
+  select {
+    width: 300px;
+  }
+  div.label {
+    width: 100px;
+    text-align: left;
+  }
+  form {
+    display: inline-block;
+    width: 400px;
+  }
+  tr.cica th {
+    background-color: rgb(128, 201, 240);
+  }
+  span.edit:hover {
+    text-shadow: 1px 1px 4px black;
+  }
+  span.edit {
+    cursor: pointer;
+    font-size: 30px;
+  }
 </style>
